ef2bdc8c6ce10ef20e1c7bf0ff1fb9381ae500b9
Roko Galić <roko.gali7@gmail.com>
2020-12-05 12:08:55 +0100

    Changed class to className

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,40 +2,40 @@ import React from 'react'
 
 function App() {
   return (
-    <div class="grid-container">
-    <header class="row">
-        <div class="logo">
+    <div className="grid-container">
+    <header className="row">
+        <div className="logo">
             <a href="index.html">Tech-@-Tack</a>
         </div>
-        <div class="header-links">
+        <div className="header-links">
             <a href="cart.html">Košarica 👜 </a>
             <a href="singin.html">Sign up/in 🖊 </a>
         </div>
           
     </header>
-    <main class="main">
-      <div class= "row center">
-          <div class="card">
+    <main className="main">
+      <div className= "row center">
+          <div className="card">
               <a href="product.html">
-                  <img class="medium" src="./images/p1.jpg" alt="product"></img>
+                  <img className="medium" src="./images/p1.jpg" alt="product"></img>
               </a>
-              <div class="card-body">
+              <div className="card-body">
                   <a href="product.html">
                       <h2>Wireless charging stand</h2>
                   </a>
-                  <div class="rating">
-                  <span> <i class="fa fa-star"></i> </span>
-                  <span> <i class="fa fa-star"></i> </span>
-                  <span> <i class="fa fa-star"></i> </span>
-                  <span> <i class="fa fa-star"></i> </span>
-                  <span> <i class="fa fa-star"></i> </span>
+                  <div className="rating">
+                  <span> <i className="fa fa-star"></i> </span>
+                  <span> <i className="fa fa-star"></i> </span>
+                  <span> <i className="fa fa-star"></i> </span>
+                  <span> <i className="fa fa-star"></i> </span>
+                  <span> <i className="fa fa-star"></i> </span>
                   </div>
               </div>
-              <div class="price">€30</div>
+              <div className="price">€30</div>
           </div>
       </div>
     </main>
-    <footer class="row center">
+    <footer className="row center">
         All rights reserved © Tech-@-Tack 2020
     </footer>
 </div>
