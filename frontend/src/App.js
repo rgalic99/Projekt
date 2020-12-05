8184421ceb15c9089609165d1692649b41c4dc39
Roko Galić <roko.gali7@gmail.com>
2020-12-05 16:15:48 +0100

    Fixed formatting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,32 @@
-import React from 'react'
-import data from './data';
-import Product from './components/Product'
+import React from "react";
+import data from "./data";
+import Product from "./components/Product";
 
 function App() {
-  return (
-    <div className="grid-container">
-    <header className="row">
-        <div className="logo">
-            <a href="index.html">Tech-@-Tack</a>
-        </div>
-        <div className="header-links">
-            <a href="cart.html">Košarica 👜 </a>
-            <a href="singin.html">Sign up/in 🖊 </a>
-        </div>
-          
-    </header>
-    <main className="main">
-        <div className= "row center">
-            {data.products.map((product)=>(
-                <Product key={product._id} product={product}></Product> 
-            ))}   
-        </div>
-    </main>
+	return (
+		<div className="grid-container">
+			<header className="row">
+				<div className="logo">
+					<a href="index.html">Tech-@-Tack</a>
+				</div>
+				<div className="header-links">
+					<a href="cart.html">Košarica 👜 </a>
+					<a href="singin.html">Sign up/in 🖊 </a>
+				</div>
+			</header>
+			<main className="main">
+				<div className="row center">
+					{data.products.map((product) => (
+						<Product key={product._id} product={product}></Product>
+					))}
+				</div>
+			</main>
 
-    <footer className="row center">
-    All rights reserved © Tech-@-Tack 2020
-    </footer>
-    </div>
-  );
+			<footer className="row center">
+				All rights reserved © Tech-@-Tack 2020
+			</footer>
+		</div>
+	);
 }
 
 export default App;
