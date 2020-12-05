7f5dacf8143c510e611a7181775b1729c9d6719b
Roko Galić <roko.gali7@gmail.com>
2020-12-05 13:41:51 +0100

    Added rating and product component to reduce copy/paste

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import data from './data';
+import Product from './components/Product'
 
 function App() {
   return (
@@ -15,33 +16,17 @@ function App() {
           
     </header>
     <main className="main">
-      <div className= "row center">
-            {
-                data.products.map((product)=>
-                <div key={product._id} className="card">
-              <a href={`/product/${product._id}`}>
-                  <img className="medium" src={product.image} alt={product.name}></img>
-              </a>
-              <div className="card-body">
-                  <a href={`/product/${product._id}`}>
-                      <h2>{product.name}</h2>
-                  </a>
-                  <div className="rating">
-                  <span> <i className="fa fa-star"></i> </span>
-                  <span> <i className="fa fa-star"></i> </span>
-                  <span> <i className="fa fa-star"></i> </span>
-                  <span> <i className="fa fa-star"></i> </span>
-                  <span> <i className="fa fa-star"></i> </span>
-                  </div>
-              </div>
-              <div className="price">€{product.price}</div>
-              </div>)}     
-      </div>
+        <div className= "row center">
+            {data.products.map((product)=>(
+                <Product key={product._id} product={product}></Product> 
+            ))}   
+        </div>
     </main>
+
     <footer className="row center">
-        All rights reserved © Tech-@-Tack 2020
+    All rights reserved © Tech-@-Tack 2020
     </footer>
-</div>
+    </div>
   );
 }
 
