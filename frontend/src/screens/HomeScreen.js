2213f69115daabb8ebee91be53e5ac7b537c39a8
Roko Galić <roko.gali7@gmail.com>
2020-12-11 20:57:11 +0100

    Added loading animation and error messaging support through axios

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,13 +1,40 @@
-import React from "react";
-import data from "../data";
+import React, { useEffect, useState } from "react";
 import Product from "../components/Product";
+import axios from "axios";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
 
 export default function HomeScreen(props) {
+	const [Loading, setLoading] = useState(false);
+	const [Error, setError] = useState(false);
+	const [products, setProducts] = useState([]);
+	useEffect(() => {
+		const fetchData = async () => {
+			setLoading(true);
+			try {
+				const { data } = await axios.get("/api/products");
+				setLoading(false);
+				setProducts(data);
+			} catch (err) {
+				setError(err.message);
+				setLoading(false);
+			}
+		};
+		fetchData();
+	}, []);
 	return (
-		<div className="row center">
-			{data.products.map((product) => (
-				<Product key={product._id} product={product}></Product>
-			))}
+		<div>
+			{Loading ? (
+				<LoadingBox></LoadingBox>
+			) : Error ? (
+				<MessageBox variant="faliure">{Error}</MessageBox>
+			) : (
+				<div className="row center">
+					{products.map((product) => (
+						<Product key={product._id} product={product}></Product>
+					))}
+				</div>
+			)}
 		</div>
 	);
 }
