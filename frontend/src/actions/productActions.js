4fc477ad0bd340ae3ca9f73f6ba579c1b40380d8
Roko Galić <roko.gali7@gmail.com>
2020-12-12 18:33:58 +0100

    Added redux to product screen

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -3,6 +3,11 @@ import {
 	PRODUCT_LIST_FALIURE,
 	PRODUCT_LIST_REQUEST,
 	PRODUCT_LIST_SUCCESS,
+} from "../constants/product0Constants";
+import {
+	PRODUCT_DETAILS_FALIURE,
+	PRODUCT_DETAILS_REQUEST,
+	PRODUCT_DETAILS_SUCCESS,
 } from "../constants/productConstants";
 
 export const listProducts = () => async (dispatch) => {
@@ -14,5 +19,21 @@ export const listProducts = () => async (dispatch) => {
 		dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
 	} catch (error) {
 		dispatch({ type: PRODUCT_LIST_FALIURE, payload: error.message });
+	}
+};
+
+export const detailsProduct = (productId) => async (dispatch) => {
+	dispatch({ type: PRODUCT_DETAILS_REQUEST, payload: productId });
+	try {
+		const { data } = await Axios.get(`/api/products/${productId}`);
+		dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
+	} catch (error) {
+		dispatch({
+			type: PRODUCT_DETAILS_FALIURE,
+			payload:
+				error.response && error.response.data.message
+					? error.response.data.message
+					: error.message,
+		});
 	}
 };
--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,4 +1,7 @@
 import {
+	PRODUCT_DETAILS_FALIURE,
+	PRODUCT_DETAILS_REQUEST,
+	PRODUCT_DETAILS_SUCCESS,
 	PRODUCT_LIST_FALIURE,
 	PRODUCT_LIST_REQUEST,
 	PRODUCT_LIST_SUCCESS,
@@ -15,6 +18,21 @@ export const productListReducer = (
 		case PRODUCT_LIST_SUCCESS:
 			return { loading: false, products: action.payload };
 		case PRODUCT_LIST_FALIURE:
+			return { loading: false, error: action.payload };
+		default:
+			return state;
+	}
+};
+export const productDetailsReducer = (
+	state = { loading: true, product: {} },
+	action
+) => {
+	switch (action.type) {
+		case PRODUCT_DETAILS_REQUEST:
+			return { loading: true };
+		case PRODUCT_DETAILS_SUCCESS:
+			return { loading: false, product: action.payload };
+		case PRODUCT_DETAILS_FALIURE:
 			return { loading: false, error: action.payload };
 		default:
 			return state;
