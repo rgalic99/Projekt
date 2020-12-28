a111bcd1046ce4598159e6bd2033dfea318e4abb
Roko Galić <roko.gali7@gmail.com>
2020-12-28 17:08:54 +0100

    Added PayPal button integrated paying through paypal fixed order returning null instead of empty object

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,9 @@ mongoose.connect("mongodb://localhost/tech-attack" || process.env.MONGODB_URL, {
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
 app.use("/api/orders", orderRouter);
+app.get("/api/config/paypal", (req, res) => {
+	res.send(process.env.PAYPAL_CLIENT_ID || "sb");
+});
 app.get("/api/users", (req, res) => {
 	res.send(data.users);
 });
--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -1,18 +1,52 @@
-import React, { useEffect } from "react";
+import Axios from "axios";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { detailsOrder } from "../actions/orderActions";
 import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
+import { PayPalButton } from "react-paypal-button-v2";
 
 export default function OrderScreen(props) {
+	const userSignin = useSelector((state) => state.userSignin);
+	const { userInfo } = userSignin;
+	if (!userInfo) {
+		props.history.push("/signin");
+	}
 	const orderId = props.match.params.id;
+	const [sdkReady, setSdkReady] = useState(false);
 	const orderDetails = useSelector((state) => state.orderDetails);
 	const { order, loading, error } = orderDetails;
 	const dispatch = useDispatch();
 	useEffect(() => {
-		dispatch(detailsOrder(orderId));
-	}, [dispatch, orderId]);
+		const addPayPalScript = async () => {
+			const { data } = await Axios.get("api/config/paypal");
+			const script = document.createElement("script");
+			script.type = "text/javascript";
+			script.src = `https://www.paypal.com/sdk/ks?client-id=${data}`;
+			script.async = true;
+			script.onload = () => {
+				setSdkReady(true);
+			};
+			document.body.appendChild(script);
+		};
+		if (!order) {
+			dispatch(detailsOrder(orderId));
+		} else {
+			if (!order.isPaid) {
+				if (!window.paypal) {
+					addPayPalScript();
+				} else {
+					setSdkReady(true);
+				}
+			}
+		}
+	}, [dispatch, order, orderId, sdkReady]);
+
+	const SuccessPaymentHandler = () => {
+		//dispatch pay order
+	};
+
 	return loading ? (
 		<LoadingBox></LoadingBox>
 	) : error ? (
@@ -125,6 +159,18 @@ export default function OrderScreen(props) {
 									</div>
 								</div>
 							</li>
+							{!order.isPaid && (
+								<li>
+									{!sdkReady ? (
+										<LoadingBox></LoadingBox>
+									) : (
+										<PayPalButton
+											amount={order.totalPrice.toFixed(0)}
+											onSuccess={SuccessPaymentHandler}
+										></PayPalButton>
+									)}
+								</li>
+							)}
 						</ul>
 					</div>
 				</div>
