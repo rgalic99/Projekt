c910cfae8114b6b7e4b27b6e78defea9bd4e63fa
Roko Galić <roko.gali7@gmail.com>
2020-12-31 13:31:13 +0100

    Added ability to choose image for creating product added mutler

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from "express";
+import path from "path";
 import mongoose from "mongoose";
 import productRouter from "./routers/productRouter.js";
 import userRouter from "./routers/userRouter.js";
 import dotenv from "dotenv";
 import orderRouter from "./routers/orderRouter.js";
+import uploadRouter from "./routers/uploadRouter.js";
 
 dotenv.config();
 
@@ -16,12 +18,17 @@ mongoose.connect("mongodb://localhost/tech-attack" || process.env.MONGODB_URL, {
 	useCreateIndex: true,
 });
 
+app.use("/api/uploads", uploadRouter);
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
 app.use("/api/orders", orderRouter);
 app.get("/api/config/paypal", (req, res) => {
 	res.send(process.env.PAYPAL_CLIENT_ID || "sb");
 });
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
 app.get("/api/users", (req, res) => {
 	res.send(data.users);
 });
