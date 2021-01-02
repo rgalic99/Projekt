219b6b1bfba82983475bfb08b50998b5aa7926ad
Roko Galić <roko.gali7@gmail.com>
2021-01-02 14:34:40 +0100

    Added Heroku

--- backend/server.js
+++ backend/server.js
@@ -32,9 +32,14 @@ app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 app.get("/api/users", (req, res) => {
 	res.send(data.users);
 });
-app.get("/", (req, res) => {
+
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.get("*", (req, res) =>
+	res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+);
+/* app.get("/", (req, res) => {
 	res.send("Server is ready");
-});
+}); */
 app.use((err, req, res, next) => {
 	res.status(500).send({ message: err.message });
 });
